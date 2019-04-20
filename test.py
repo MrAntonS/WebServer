52436752ebbe606d4c6558f72a654e762f608342
MrAntonS <45099803+MrAntonS@users.noreply.github.com>
2019-04-20 12:23:44 +0300

    aga
    aga

--- test.py
+++ test.py
@@ -4,12 +4,14 @@ def test():
     api = OpenSkyApi()
     count = []
     # bbox = (min latitude, max latitude, min longitude, max longitude)
-    states = api.get_states(icao24=("155be4", "155c0e", "155c0c", "155c0a", "155c16", "155bc1", "155c15", "155be7", "155bd3", "155c0d"))
+    states = api.get_states()
+    counts = 1
     for s in states.states:
-        count.append([s.icao24, s.callsign, s.last_contact, s.longitude, s.latitude, s.velocity, s.geo_altitude, s.heading, s.on_ground])
-        x = ""
-        counts = 0
-        for i in count:
+        if "AFL" in s.callsign:
+            count.append([s.icao24, s.callsign, s.last_contact, s.longitude, s.latitude, s.velocity, s.geo_altitude, s.heading, s.on_ground, counts])
             counts += 1
-            x = x + str(i[3]) + "," + str(i[4]) + "," + str(counts) + "~"   
+        x = ""
+        for i in count:
+            x = x + str(i[3]) + "," + str(i[4]) + "," + str(i[9]) + "~"   
+            print(x)
     return [count, x]
