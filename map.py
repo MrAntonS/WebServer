670004729892d4b3ad29f6d9c8e8980bba0f598b
MrAntonS <45099803+MrAntonS@users.noreply.github.com>
2019-04-26 16:53:42 +0300

    Последний на сегодня коммит проекта.
    Добавлена возможность узнать хар-ки самолета.

--- map.py
+++ map.py
@@ -1,19 +1,22 @@
 import requests
 import test
+import os
 
+#создаем карту
 geocoder_api_server = "http://static-maps.yandex.ru/1.x/"
-def get_map(x, y, zoom, filename, size=450, map_type="map"):
+def get_map(x, y, zoom, filename, slovar, size=450, map_type="map"):
     params = {"l": map_type,
               "z": str(zoom),
               "ll": ",".join([str(x),str(y)]),
               "size": ",".join([str(500), str(400)]),
-              "pt": test.test()[1][:-1]}
+              "pt": slovar}
     response = requests.get(geocoder_api_server, params=params)
     if not response:
         err = "Http статус:"+str(response.status_code)+"("+str(response.reason)+")"
         return False, err
     
     try:
+        # Передаем
         with open(filename, "wb") as file:
             file.write(response.content)
     except IOError as ex:
--- test.py
+++ test.py
@@ -1,17 +1,20 @@
 from opensky_api import OpenSkyApi
 
 def test():
+    #объект всех самолетов
     api = OpenSkyApi()
     count = []
     # bbox = (min latitude, max latitude, min longitude, max longitude)
     states = api.get_states()
+    #присвоение каждому самолету номер
+    #для того чтобы в телеграме узнать информацию о нем
     counts = 1
     for s in states.states:
         if "AFL" in s.callsign:
-            count.append([s.icao24, s.callsign, s.last_contact, s.longitude, s.latitude, s.velocity, s.geo_altitude, s.heading, s.on_ground, counts])
+            count.append([s.icao24, s.callsign, s.last_contact, s.longitude, s.latitude, s.velocity, s.geo_altitude, s.heading, s.on_ground, counts, s.origin_country])
             counts += 1
         x = ""
+        #формировка запроса для карт (обозначение меток)
         for i in count:
             x = x + str(i[3]) + "," + str(i[4]) + "," + str(i[9]) + "~"   
-            print(x)
     return [count, x]
